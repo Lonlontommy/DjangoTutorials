1476aa98244ec1ae2f1ffe05da97ecb03e54cc1c
Lonlontommy <heisann852@hotmail.com>
2018-02-06 18:22:07 +0100

    Added pagination

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,34 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 from .models import Post
+from .forms import EmailPostForm
 
 # Create your views here.
 # Retrieves a web request and returns a web response. Inside view goes all the the logic to return desired response. First create application view, define url pattern for each view, and then templates = Returns http response.
 
-def post_list(request):
-	posts = Post.published.all()
-	return render(request, 'blog/post/list.html', {'posts': posts})
+def post_list(request, category=None):
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3) # 3 posts in each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'page': page,
+                                                   'posts': posts})
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
@@ -16,4 +38,25 @@ def post_detail(request, year, month, day, post):
                                    publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
 
+def post_share(request, post_id):
+    # Retrieve post by ID
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = '{} ({}) recommends you reading "{}"'.format(cd['name'], cd['email'], post.title)
+            message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(post.title, post_url, cd['name'], cd['comments'])
+            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form,
+                                                    'sent': sent})
 
